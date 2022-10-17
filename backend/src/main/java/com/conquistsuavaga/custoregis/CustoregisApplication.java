f015934ea80f6f568dab091f9a54ddfe219bda79
Felipe Sousa <felipe.fps09@hotmail.com>
2022-10-16 22:17:37 -0300

    Initialize project

---
+++ backend/src/main/java/com/conquistsuavaga/custoregis/CustoregisApplication.java
@@ -0,0 +1,13 @@
+package com.conquistsuavaga.custoregis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustoregisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustoregisApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/conquistsuavaga/custoregis/CustoregisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conquistsuavaga.custoregis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustoregisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
